ac51f085f7e5812bac1a747e1fa9fa0e5e323249
QueenCurry <yeawonjo@gmail.com>
2019-02-08 02:11:04 +0000

    1. 해상도 고정 > UI 배치 이상한 문제 수정

---
+++ Assets/Script/StartScene/ScreenSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(Screen.width, (Screen.width * 16) / 9, true);
+    }
+}
