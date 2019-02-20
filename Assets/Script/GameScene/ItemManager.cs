959219356948c38ba6013fa4d47b2b236a0c418a
정유진 <crm05265@gmail.com>
2019-02-20 10:19:18 +0000

    No Comments

--- Assets/Script/GameScene/ItemManager.cs
+++ Assets/Script/GameScene/ItemManager.cs
@@ -98,66 +98,80 @@ public class ItemManager : MonoBehaviour
         }
         else if (catmove.firstPlate == 1 && collision.gameObject.CompareTag("PlateCollider"))
         {
-            if (Item.CompareTag("itemgreen"))   //초록 접시 초밥 방았을때
-            {
-                //모드체크
-                if (catmove.Modenumber != 1 && catmove.Modenumber != 12 && catmove.Modenumber != 13 && catmove.Modenumber != 123)
-                {
-                    GameOver.GameIsOver = true;
-                }
-                else
-                {
-                    Item.GetComponent<SushiFalling>().falling = false;
-                    Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
-                    Debug.Log("Tagged");
-                    ScoreManager.GetComponent<ScoreManager>().ScoreUp(this.itemScore);
-                }
-            }
-            else if (Item.CompareTag("itemblue"))   //파랑 접시 초밥 받았을 때
-            {
-                if (catmove.Modenumber != 2 && catmove.Modenumber != 12 && catmove.Modenumber != 23 && catmove.Modenumber != 123)
-                {
-                    GameOver.GameIsOver = true;
-                }
-                else
-                {
-                    Item.GetComponent<SushiFalling>().falling = false;
-                    Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
-                    Debug.Log("Tagged");
-                    ScoreManager.GetComponent<ScoreManager>().ScoreUp(this.itemScore);
-                }
 
-            }
-            else if (Item.CompareTag("itemred"))    //빨강 접시 초밥 받았을 때
-            {
-                if (catmove.Modenumber != 3 && catmove.Modenumber != 13 && catmove.Modenumber != 23 && catmove.Modenumber != 123)
-                {
-                    GameOver.GameIsOver = true;
-                }
-                else
-                {
-                    Item.GetComponent<SushiFalling>().falling = false;
-                    Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
-                    Debug.Log("Tagged");
-                    ScoreManager.GetComponent<ScoreManager>().ScoreUp(this.itemScore);
-                }
-            }
-            else if (Item.CompareTag("turtle"))     //거북이 받았을떄
-            {
-                Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
-                itemFalling.turtlePower();
-            }
-            else if (Item.CompareTag("gook"))
-            { //국 받았을 때
-                Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
-                itemFalling.gookPower();
-            }
-            else if (Item.CompareTag("RainbowPlate"))
-            {
-                Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
-                DragCat.DishScore *= 2; // 피버타임때 점수가 2배 되므로 접시를 받을때마다 2갰기 받은 것으로 처리
-                // 초밥 점수 2배로 하는 코드 필요
 
+            GameObject ob = collision.gameObject.transform.parent.gameObject;
+            DishFalling dF = ob.GetComponent<DishFalling>();
+
+            int count = 0;
+
+            if (dF.dishNum == 1) { count = ob.GetComponent<GreenPlate>().Count; }
+            else if (dF.dishNum == 2) { count = ob.GetComponent<BluePlate>().Count; }
+            else if (dF.dishNum == 3) { count = ob.GetComponent<RedPlate>().Count; }
+
+            if (count == 1)
+            {
+                if (Item.CompareTag("itemgreen"))   //초록 접시 초밥 방았을때
+                {
+                    //모드체크
+                    if (catmove.Modenumber != 1 && catmove.Modenumber != 12 && catmove.Modenumber != 13 && catmove.Modenumber != 123)
+                    {
+                        GameOver.GameIsOver = true;
+                    }
+                    else
+                    {
+                        Item.GetComponent<SushiFalling>().falling = false;
+                        Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
+                        Debug.Log("Tagged");
+                        ScoreManager.GetComponent<ScoreManager>().ScoreUp(this.itemScore);
+                    }
+                }
+                else if (Item.CompareTag("itemblue"))   //파랑 접시 초밥 받았을 때
+                {
+                    if (catmove.Modenumber != 2 && catmove.Modenumber != 12 && catmove.Modenumber != 23 && catmove.Modenumber != 123)
+                    {
+                        GameOver.GameIsOver = true;
+                    }
+                    else
+                    {
+                        Item.GetComponent<SushiFalling>().falling = false;
+                        Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
+                        Debug.Log("Tagged");
+                        ScoreManager.GetComponent<ScoreManager>().ScoreUp(this.itemScore);
+                    }
+
+                }
+                else if (Item.CompareTag("itemred"))    //빨강 접시 초밥 받았을 때
+                {
+                    if (catmove.Modenumber != 3 && catmove.Modenumber != 13 && catmove.Modenumber != 23 && catmove.Modenumber != 123)
+                    {
+                        GameOver.GameIsOver = true;
+                    }
+                    else
+                    {
+                        Item.GetComponent<SushiFalling>().falling = false;
+                        Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
+                        Debug.Log("Tagged");
+                        ScoreManager.GetComponent<ScoreManager>().ScoreUp(this.itemScore);
+                    }
+                }
+                else if (Item.CompareTag("turtle"))     //거북이 받았을떄
+                {
+                    Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
+                    itemFalling.turtlePower();
+                }
+                else if (Item.CompareTag("gook"))
+                { //국 받았을 때
+                    Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
+                    itemFalling.gookPower();
+                }
+                else if (Item.CompareTag("RainbowPlate"))
+                {
+                    Item.transform.localPosition = new Vector3(Random.Range(-520f, 520f), 2550 / 2, 0);
+                    DragCat.DishScore *= 2; // 피버타임때 점수가 2배 되므로 접시를 받을때마다 2갰기 받은 것으로 처리
+                                            // 초밥 점수 2배로 하는 코드 필요
+
+                }
             }
 
             //점수에 아이템 점수 더함
