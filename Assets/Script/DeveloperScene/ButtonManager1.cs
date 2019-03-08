3aa5dfd701f4b5fcad17fc8bbd7a75e65447e051
QueenCurry <yeawonjo@gmail.com>
2019-03-08 12:32:16 +0000

    1. 초밥칭호 언락 바꿀수 있는 개발자 모드 추가

---
+++ Assets/Script/DeveloperScene/ButtonManager1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager1 : MonoBehaviour
+{
+    public void GoMain()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ChangeTo1()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 1;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo2()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 2;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo3()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 3;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo4()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 4;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo5()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 5;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo7()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 7;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo8()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 8;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo9()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 9;
+        PlayerDataLoad.SaveData();
+    }
+
+    public void ChangeTo6()
+    {
+        PlayerDataLoad.playerdata.AchievementIndex = 6;
+        PlayerDataLoad.SaveData();
+    }
+}
